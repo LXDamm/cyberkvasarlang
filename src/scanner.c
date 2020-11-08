69567e9088d650d43c00795a85c1c12243e38f66
Lexx Utopia Olivia Damm <lexx.damm@gmail.com>
2020-11-08 19:48:12 +0100

    Do and end works

--- src/scanner.c
+++ src/scanner.c
@@ -17,3 +17,150 @@ void initScanner(const char* source) {
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool isAlpha(char c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static char peek() {
+    return *scanner.current;
+}
+
+static char peekNext() {
+    if (isAtEnd()) return '\0';
+    return scanner.current[1];
+}
+
+static bool match(char expected) {
+    if (isAtEnd()) return false;
+    if(*scanner.current != expected) return false;
+    scanner.current++;
+    return true;
+}
+
+static bool isDo(char c) {
+    if (c != 'd') return false;
+    if (!match('o')) return false;
+    return true;
+}
+
+static bool isEnd(char c) {
+    if (c != 'e') return false;
+    if (!match('n')) return false;
+    if (!match('d')) return false;
+    return true;
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token errorToken(const char* message) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static void skipWhitespace() {
+    for (;;) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t': {
+                advance();
+                break;
+            }
+            case '\n': {
+                scanner.line++;
+                advance();
+                break;
+            }
+            case '/': {
+                if (peekNext() == '/') {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else return;
+                break;
+            }
+            default: return;
+        }
+    }
+}
+
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+        advance();
+
+        while(isDigit(peek())) advance();
+    }
+
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+    while (peek() != '\'' && !isAtEnd()) {
+        if (peek() == '\n') scanner.line++;
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
+Token scanToken() {
+    skipWhitespace();
+
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    char c = advance();
+    if (isAlpha(c)) return identifier();
+    if (isDigit(c)) return number();
+
+    if (isDo(c)) return makeToken(TOKEN_DO);
+    if (isEnd(c)) return makeToken(TOKEN_END);
+
+    switch (c) {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case ':': return makeToken(TOKEN_COLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case '=': return makeToken(TOKEN_EQUAL);
+        case 39: return string();
+    }
+
+    return errorToken("Unexpected character. ");
+}
--- src/scanner.h
+++ src/scanner.h
@@ -1,6 +1,24 @@
 #ifndef cyberkvasar_scanner_h
 #define cyberkvasar_scanner_h
 
+typedef enum {
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+    TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+    TOKEN_SLASH, TOKEN_STAR, TOKEN_COLON, TOKEN_EQUAL,
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+    TOKEN_DO, TOKEN_END,
+    TOKEN_RETURN, TOKEN_DEF, TOKEN_NULL,
+    TOKEN_ERROR, TOKEN_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    const char* start;
+    int length;
+    int line;
+} Token;
+
 void initScanner(const char* source);
+Token scanToken();
 
 #endif
